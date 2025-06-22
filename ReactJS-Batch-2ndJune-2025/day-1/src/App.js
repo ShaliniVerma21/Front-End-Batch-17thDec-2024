b230e456cb391bdbbaccc6eff6b9ddbf6f30f578
Shalini Verma <46881467+ShaliniVerma21@users.noreply.github.com>
2025-06-22 14:41:59 +0530

    Add files via upload

---
+++ ReactJS-Batch-2ndJune-2025/day-1/src/App.js
@@ -0,0 +1,37 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Welcome to react js Session-1</h1>
+        <p>
+          React is one of the most popular, efficient, and powerful open source,
+          front-end JavaScript library for building dynamic and interactive user
+          interface (UI) in web application. ReactJS is not a framework, it is
+          just a library developed by Facebook. It is a simple, feature rich and
+          component based UI library. Used to make single page application (Not
+          needed to reload everytime). React develops applications by creating
+          various reusable and independent codes Known as Components. ReactJS
+          can be used to develop small applications as well as big, complex
+          applications. React community compliments React library by providing
+          large set of ready-made components to develop web application in a
+          record time. React community also provides advanced concept like state
+          management, routing, etc., on top of the React library. The main
+          objective of ReactJS is to develop User Interfaces (UI) that improves
+          the speed of the apps. It uses virtual DOM (JavaScript object), which
+          improves the performance of the app. The JavaScript virtual DOM is
+          faster than the regular DOM. React "reacts" to state changes in your
+          components quickly and automatically to re-render the components in
+          the HTML DOM by utilizing the virtual DOM. The virtual DOM is an
+          in-memory representation of an actual DOM. By doing most of the
+          processing inside the virtual DOM rather than directly in the
+          browser's DOM, React can act quickly and only add, update, and remove
+          components which have changed since the last render cycle occurred.
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
