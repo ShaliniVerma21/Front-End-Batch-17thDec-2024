66d62a014ca5a4fc480ca5d4bbd42cab8e4733a0
Shalini Verma <46881467+ShaliniVerma21@users.noreply.github.com>
2025-06-28 17:32:40 +0530

    Add files via upload

---
+++ ReactJS-Batch-2ndJune-2025/day-4/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Footer from './MyComponents/Footer';
+import Header from './MyComponents/Header';
+import Main from './MyComponents/Main';
+//create a website topic : Desi Food
+function App() {
+  return (
+   <>
+   <Header/>
+   <Main/>
+   <Footer/>
+   </>
+  );
+}
+
+export default App;
---
+++ ReactJS-Batch-2ndJune-2025/day-4/src/MyComponents/Main.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './CSS/Main.css';
+import img1 from './Images/image.png';
+export default function Main() {
+  return (
+    <>
+    Copy to BlackBox<div id="carouselExample" class="carousel slide">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src={img1} class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src={img1} class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src={img1} class="d-block w-100" alt="..."/>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+    </>
+  );
+}
